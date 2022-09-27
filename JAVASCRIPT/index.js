58cfe50ab1050de858f51a37090614cad201b390
Omo-bolaji <bolajiomotayo48@gmail.com>
2022-09-27 11:20:58 +0100

    i want to commit

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,1 +1,11 @@
-let ball = "red";
+let ball = prompt("red");
+if (ball === "blue") {
+    alert("wrong ball");
+    alert("eew");
+}
+else if(ball === "green") {
+    alert("what the hell");
+}
+else{
+    alert("not a ball");
+} 
